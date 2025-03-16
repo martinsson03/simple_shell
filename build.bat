869ac84d85237d868ccd1d14ae00b3741c04bf1d
Martin Martinsson <martinsson.martin03@gmail.com>
2025-03-16 20:04:15 +0100

    Lost commit

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 if not exist build mkdir build
-gcc -O0 -g src\main.c -o build\simple_shell.exe -Wall
+gcc -O0 -g src\*.c  -I.\headers -o build\simple_shell.exe -Wall
---
+++ src/debug.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * @brief Function do view buffer contents as text
+ */
+void print_line(char* line){
+    printf("Buffer internals: \n");
+    int i = 0;
+    while(line[i] != '\0'){
+        printf("%c", line[i]);
+        i++;
+    }
+    printf("\n");
+}
